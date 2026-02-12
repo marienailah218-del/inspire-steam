3ccb0b5b3ed5ad2660c37add5eba238f8d7b1e06
marienailah218-del <marienailah218@gmail.com>
2026-02-12 10:57:16 +0300

    string_formating

---
+++ python-basics/numbers.py
@@ -0,0 +1,29 @@
+#Name: Nailah Wanjiku
+#Date: 11/2/2026
+#Program to add numbers
+
+
+first_number=20
+second_number=90
+
+
+#sum_numbers = first_number+second_number
+#difference_numbers = first_number-second_number
+#product_numbers = first_number*second_number
+#quotient_numbers = first_number/second_number
+
+
+#print(f"the sum of the numbers: {sum_numbers}")
+#print(f"the difference of the numbers:{difference_numbers}")
+#print(f"the product of the numbers:{product_numbers}")
+#print(f"the quotient of the numbers:{quotient_numbers}")
+
+
+third_number=input("what is the third number")
+
+print(f"the type of third number is:{type(third_number)}")
+
+#convert third number to an integer
+third_number = int(third_number)
+
+print(f"the type of third number is:{type(third_number)} ")
---
+++ python-basics/user_input.py
@@ -0,0 +1,19 @@
+#Name: Nailah Wanjiku
+#Date: 11/2/2026
+#Program to add,subtract,divide and multiply numbers
+
+
+first_number=20
+second_number=90
+
+
+first_number+second_number == "sum_number"
+first_number-second_number == "difference_number"
+first_number*second_number == "product_number"
+first_number/second_number == "quotient_number"
+
+
+print(int(f"sum_number"))
+print(int(f"difference_number"))
+print(int(f"product_number"))
+print(int(f"quotient_number"))
